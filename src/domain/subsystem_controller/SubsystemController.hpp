fe995dc27f614ae4c71c4edc0f423c82626c37dd
Rubén Pérez Mercado <ruben.perezmercado@alum.uca.es>
2026-01-26 11:18:03 +0100

    feat: add subsystem controller

---
+++ src/domain/subsystem_controller/SubsystemController.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "LoadStatus.hpp"
+#include "PowerCommand.hpp"
+
+/**
+ * @brief SubsystemController
+ *
+ * @details
+ * Provides the operational context and load status to the PowerManager.
+ * In a real system, this module would interface with hardware controllers
+ * or other subsystems. In simulation, it can be fed with predefined scenarios.
+ */
+class SubsystemController {
+    public:
+        /**
+        * @brief Constructor
+        */
+        SubsystemController();
+
+        /**
+        * @brief Destructor
+        */
+        ~SubsystemController() = default;
+
+        /**
+        * @brief Update the current load status of the subsystem
+        *
+        * @param command The command issued by the Power Manager
+        */
+        void updateLoadStatus(const PowerCommand& command);
+
+        /**
+        * @brief Retrieve the latest load status for the PowerManager
+        *
+        * @return Current LoadStatus
+        */
+        LoadStatus getLoadStatus() const;
+
+    private:
+        // Last known load status
+        LoadStatus m_loadStatus;
+
+        // Setters for each power mode
+        void setNormal();
+        void setSafe();
+        void setOff(const bool emergencyShutdown);
+
+        // Simulation of safe shutdown
+        void wait_shutdown() const;
+};
+
