57d5db3e79c317e063d24b5407fa7fc379a49350
Rubén Pérez Mercado <ruben.perezmercado@alum.uca.es>
2026-01-26 11:20:14 +0100

    feat: add sample main

---
+++ src/app/main.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+
+#include "Config.hpp"
+#include "BatteryMonitor.hpp"
+
+int main() {
+    Config config("../cfg/const.cfg");
+    BatteryMonitor batteryMonitor(config);
+
+    for (int i = 0; i < 25; ++i) {
+    batteryMonitor.acquireMeasurements();
+    auto measurements = batteryMonitor.getMeasurements();
+    if (measurements) {
+        std::cout << "Voltage: " << measurements->voltage << " V\n";
+        std::cout << "Current: " << measurements->current << " A\n";
+        std::cout << "Temperature: " << measurements->temperature << " °C\n";
+        std::cout << "Timestamp: " << measurements->timestamp_us << " us\n";
+    } else {
+        std::cout << "No measurements available.\n";
+    }
+
+    auto healthStatus = batteryMonitor.getSensorHealthStatus();
+    std::cout << "Sensor ID: " << static_cast<int>(healthStatus.sourceId) << "\n";
+    std::cout << "Sensor Status: " << static_cast<int>(healthStatus.status) << "\n";
+    std::cout << "Health Timestamp: " << healthStatus.timestamp_us << " us\n";
+
+    std::cout << "-------------------------\n";
+    }
+    return 0;
+}
