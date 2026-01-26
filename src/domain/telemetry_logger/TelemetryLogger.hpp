84c7dc2c2ee301bea77bfeee85c000699ce4c9eb
Rubén Pérez Mercado <ruben.perezmercado@alum.uca.es>
2026-01-26 11:18:28 +0100

    feat: add telemetry logger

---
+++ src/domain/telemetry_logger/TelemetryLogger.hpp
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <ostream>
+
+#include "BatteryMeasurements.hpp"
+#include "SensorHealthStatus.hpp"
+#include "LocalSensorHealthStatus.hpp"
+#include "LoadStatus.hpp"
+#include "PowerCommand.hpp"
+
+/**
+ * @brief TelemetryLogger
+ *
+ * @details
+ * Collects telemetry data and allows printing to a user-specified output stream.
+ * By default, telemetry is printed to standard output.
+ */
+class TelemetryLogger {
+    public:
+        /**
+        * @brief Constructor
+        *
+        * @param outputStream Stream to write telemetry to (default: std::cout)
+        */
+        explicit TelemetryLogger(std::ostream& outputStream);
+
+        ~TelemetryLogger() = default;
+
+        /**
+        * @brief Log battery measurements
+        *
+        * @param measurements Current measurements from a BatteryMonitor
+        */
+        void logBatteryMeasurements(const BatteryMeasurements& measurements);
+
+        /**
+        * @brief Log local sensor health statuses
+        *
+        * @param status Current local health status
+        */
+        void logLocalSensorHealthStatus(const LocalSensorHealthStatus& status);
+
+        /**
+        * @brief Log sensor health statuses
+        *
+        * @param status Current consolidated health status
+        */
+        void logSensorHealthStatus(const SensorHealthStatus& status);
+
+        /**
+        * @brief Log load status
+        *
+        * @param load Current load status
+        */
+        void logLoadStatus(const LoadStatus& load);
+
+        /**
+        * @brief log power command
+        *
+        * @param command Current power command
+        */
+        void logPowerCommand(const PowerCommand& command);
+
+        /**
+        * @brief Clear all stored telemetry
+        */
+        void reset();
+
+    private:
+        // Output stream for telemetry
+        std::ostream& m_out;
+
+        // Enum Names
+        const char* sensorStateEnumNames[3] = {"NOMINAL", "DEGRADED", "FAILED"};
+        const char* powerModeEnumNames[3] = {"OFF", "SAFE", "NORMAL"};
+
+        const std::string getOnOffStateFromBoolean(const bool state);
+};
+
