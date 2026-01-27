632e2ca3cf50c2b721f4c86621f0f8ae548e45d6
Rubén Pérez Mercado <ruben.perezmercado@alum.uca.es>
2026-01-27 16:00:32 +0100

    fix: solve flags issues in power manager

--- src/domain/power_manager/PowerManager.cpp
+++ src/domain/power_manager/PowerManager.cpp
@@ -3,9 +3,10 @@
 #include "PowerManager.hpp"
 #include "Log.hpp"
 
-PowerManager::PowerManager(const Config& config) : config(config), m_lastMeasurements(std::nullopt), 
-    m_lastHealthStatus(std::nullopt), m_overcurrentStartTimestamp(std::nullopt),
-    m_isShutdown(false), m_currentMode(PowerMode::NORMAL), m_lastCommand(std::nullopt) {}
+PowerManager::PowerManager(const Config& config) : config(config), m_lastMeasurements(), m_lastHealthStatus(),
+    m_overcurrentStartTimestamp(), m_isShutdown(false), m_currentMode(PowerMode::NORMAL), m_voltageOverload(false),
+    m_voltageUnderload(false), m_currentOverload(false), m_sustainedOvercurrent(false), m_overtemperature(false),
+    m_undertemperature(false), m_lastCommand() {}
 
 void PowerManager::processBatteryMeasurements(const BatteryMeasurements& measurements){
         m_lastMeasurements = measurements;
@@ -34,7 +35,7 @@ void PowerManager::setCurrentFlags(){
         } else {
             m_overcurrentStartTimestamp = m_lastMeasurements.value().timestamp_us;
         }
-        } else {
+    } else {
         m_sustainedOvercurrent = false;
         m_overcurrentStartTimestamp = std::nullopt;
     }
@@ -70,7 +71,7 @@ void PowerManager::evaluate(){
 }
 
 bool PowerManager::checkOffConditions() const {
-    return m_lastHealthStatus.has_value() && m_lastHealthStatus.value().anyFailed;
+    return m_isShutdown || (m_lastHealthStatus.has_value() && m_lastHealthStatus.value().anyFailed);
 }
 
 bool PowerManager::checkSafeConditions() const {
